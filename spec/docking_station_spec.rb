6cd06630a5f4c4c5df3e5111bfd40c645d00ba4d
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-02 15:29:02 +0100

    Deleted unecessary line

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,11 @@
 # testing the docking station
 require 'docking_station'
-require 'bike'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
   it 'runs method to release bike' do
     bike = subject.release_bike
-    expect(bike).to be_working 
+    expect(bike).to be_working
   end
 end
