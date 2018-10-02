7f28b0e683c1084679cce45865b3c5186abe370d
RyanWolfen7 <wolfen7lada@gmail.com>
2018-10-02 14:30:08 +0100

    Figuring out the rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,8 @@
 # testing the docking station
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike } 
+  it { is_expected.to respond_to :release_bike }
+  it { is_expected(DockingStation.release_bike).to respond_to :working? == true}
 end
