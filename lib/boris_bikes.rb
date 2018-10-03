c1516037209cfed2b98cab67bdd760f3c2405d31
Raylene <smith.raylene@gmail.com>
2018-10-03 16:23:43 +0100

    Added docking_bike method and attribute reader

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,7 +1,14 @@
 require_relative 'bikes'
 
 class DockingStation
+
+  attr_accessor :bike
+
   def release_bike
     Bike.new
+  end
+
+  def docking_bike(bike)
+    @bike = bike
   end
 end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe DockingStation do
 	it 'releases a bike' do
 		expect(DockingStation.new).to respond_to(:release_bike)
   end
+
+  it 'returns a bike' do
+    bike = Bike.new
+    subject.docking_bike(bike)
+    expect(subject.bike).to eq bike
+  end
+
+ # it {is_expected.to respond_to(:docking_bike).with(1).argument}
 end
