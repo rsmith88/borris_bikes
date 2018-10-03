0bddc5664707b0ea0ba9aa95eb923afa3cc0780f
Alfie Freeman <runticle@gmail.com>
2018-10-03 14:22:28 +0100

    passed rspec bike working? method

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -1,7 +1,15 @@
+
 class DockingStation
   def release_bike
+    Bike.new
+  end
+end
+
+class Bike
+  def working?
     
   end
 end
-docking_station = DockingStation.new
-docking_station.release_bike
+
+# docking_station = DockingStation.new
+# docking_station.release_bike
--- spec/boris_bikes_spec.rb
+++ spec/boris_bikes_spec.rb
@@ -1,10 +1,13 @@
 require 'boris_bikes'
-	RSpec.describe DockingStation do
+
+RSpec.describe DockingStation do
 	it 'releases a bike' do
 		expect(DockingStation.new).to respond_to(:release_bike)
   end
 end
+
 RSpec.describe Bike do
-  it "creates instance of a bike class"
-  expect(Bike.new).to respond_to(:working)
+  it "creates instance of a bike class" do
+  	expect(Bike.new).to respond_to(:working?)
+	end
 end
